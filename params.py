46726866dea2a07fd1cbfafb2fdf261ed61575e5
Timur Khafizov <timur.khafizov@bostongene.com>
2024-02-07 13:41:35 +0400

    rename .env

--- params.py
+++ params.py
@@ -12,7 +12,7 @@ logger.remove(0)
 logger.add(sink="logs/log.log", level="INFO", format=LOGGER_FORMAT, backtrace=False, diagnose=False, rotation='1 GB')
 logger.add(sys.stdout, level="INFO", format=LOGGER_FORMAT, backtrace=False, diagnose=False)
 
-DOTENV_PATH = ".env"
+DOTENV_PATH = "dev.env"
 
 load_dotenv(dotenv_path=DOTENV_PATH)
 logger.info(f"Loaded envs from {DOTENV_PATH}: {dotenv_values(dotenv_path=DOTENV_PATH)}")
